d2b29c9dae408122cb347cdb7a81a93c0e605520
Dillon H <43145893+dillonkh@users.noreply.github.com>
2020-03-11 15:28:40 -0600

    initial commit

---
+++ app/src/main/java/edu/byu/cs/tweeter/client/net/ServerFacade.java
@@ -0,0 +1,31 @@
+package edu.byu.cs.tweeter.client.net;
+
+import java.io.IOException;
+
+import edu.byu.cs.tweeter.model.service.request.FollowingRequest;
+import edu.byu.cs.tweeter.model.service.response.FollowingResponse;
+
+/**
+ * Acts as a Facade to the Tweeter server. All network requests to the server should go through
+ * this class.
+ */
+public class ServerFacade {
+
+    // TODO: Set this the the invoke URL of your API. Find it by going to your API in AWS, clicking
+    //  on stages in the right-side menu, and clicking on the stage you deployed your API to.
+    private static final String SERVER_URL = "Insert your API invoke URL here";
+
+    /**
+     * Returns the users that the user specified in the request is following. Uses information in
+     * the request object to limit the number of followees returned and to return the next set of
+     * followees after any that were returned in a previous request.
+     *
+     * @param request contains information about the user whose followees are to be returned and any
+     *                other information required to satisfy the request.
+     * @return the followees.
+     */
+    public FollowingResponse getFollowees(FollowingRequest request, String urlPath) throws IOException {
+        ClientCommunicator clientCommunicator = new ClientCommunicator(SERVER_URL);
+        return clientCommunicator.doPost(urlPath, request, null, FollowingResponse.class);
+    }
+}
